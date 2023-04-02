7f4ae20f89d01dca2852ba27f49adf41f3de9127
Wan <hermawan0797@gmail.com>
2023-04-03 04:41:17 +0700

    Finish Challange

--- app_gr.py
+++ app_gr.py
@@ -2,47 +2,67 @@
 import gradio as gr
 import pandas as pd
 import re
+import sqlite3
 
-# Mengambil data abusive dan kamusalay
-df_abusive = pd.read_csv("abusive.csv")
-df_alay = pd.read_csv('new_kamusalay.csv',encoding='latin-1',names=['alay','normal'])
+# Fungsi Cleansing
 
-# Menentukan fungsi Pembersihan Teks 
-def text_cleansing(text):
-    # Replace kata abusive dengan spasi
-    abusive_words = df_abusive["ABUSIVE"].tolist()
-    for word in abusive_words:
-        text = re.sub(r'\b{}\b'.format(word), ' ', text, flags=re.IGNORECASE)
+# Ubah semua ke huruf kecil
+def lowercase(s):
+    return s.lower()
+
+# Hapus semua tanda baca dan kata yang tidak di perlukan
+def punctuation(s):
+    s = re.sub('[^0-9a-zA-Z]+', ' ', s) #menghilangkan semua karakter yang bukan huruf atau angka dan menggantinya dengan spasi.
+    s = re.sub('^rt',' ', s) #menghapus awalan rt
+    s = re.sub('gue','saya', s) # Mengganti kata "gue" dengan kata "saya"
+    s = re.sub(r'\d+', '', s) #menghapus semua angka
+    s = re.sub('user',' ', s) #menghapus kata 'user'
+    s = re.sub(r':', ' ', s) #menggantikan karakter : dengan spasi
+    s = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ', s) #menghapus semua URL 
+    s = re.sub(' +', ' ', s) #menggantikan satu atau lebih spasi berturut-turut dengan satu spasi 
+    s = re.sub('\n',' ',s) #menggantikan karakter newline (\n) dengan spasi 
+    s = re.sub(r'pic.twitter.com.[\w]+', ' ', s) #menghapus semua tautan Twitter (pic.twitter.com)
+    return s
+
+# Load data abusive
+db = sqlite3.connect('database_abusive.db', check_same_thread=False)
+q_abusive = 'SELECT * FROM abusive'
+t_abusive = pd.read_sql_query(q_abusive, db)
+
+# Hapus data abusive
+def remove_abusive_words(s):
+    abusive_words = set(t_abusive['ABUSIVE'])
+    words = s.split()
+    filtered_words = [word for word in words if word not in abusive_words]
+    return ' '.join(filtered_words)
+
+# Load data kamus alay
+db = sqlite3.connect('database_alay.db', check_same_thread = False)
+q_kamusalay = 'SELECT * FROM kamusalay'
+t_kamusalay = pd.read_sql_query(q_kamusalay, db)
+alay_dict = dict(zip(t_kamusalay['alay'], t_kamusalay['normal']))
+
+# Replace Kata Alay ke Kata Normal
+def alay_to_normal(s):
+    for word in alay_dict:
+        return ' '.join([alay_dict[word] if word in alay_dict else word for word in s.split(' ')])
     
-    # Replace kata alay dengan kata normal
-    alay_words = df_alay["alay"].tolist()
-    normal_words = df_alay["normal"].tolist()
-    for i in range(len(alay_words)):
-        text = re.sub(r'\b{}\b'.format(alay_words[i]), normal_words[i], text, flags=re.IGNORECASE)
-    
-    # Membersihkan teks dengan fungsi Regex
-    text = re.sub('[^0-9a-zA-Z]+', ' ', text) # Menghapus semua karakter yang bukan angka atau huruf, lalu menggantinya dengan spasi.
-    text = re.sub(r':', '', text) # Menghapus semua tanda titik dua (:).
-    text = re.sub('\n',' ',text) # Mengganti karakter newline (\n) dengan spasi.
-    text = re.sub('rt',' ', text) # Menghapus kata "rt".
-    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ', text) # Menghapus semua URL.
-    text = re.sub(' +', ' ', text) # Mengganti dua atau lebih spasi berturut-turut dengan satu spasi.
-    text = re.sub(r'pic.twitter.com.[\w]+', '', text) # Menghapus semua URL dari situs Twitter.
-    text = re.sub('user',' ', text) # Menghapus kata "user".
-    text = re.sub('gue','saya', text) # Mengganti kata "gue" dengan kata "saya".
-    text = re.sub(r'‚Ä¶', '', text) # Menghapus karakter khusus yang tidak dikenali.
-    text = re.sub(r'\s+', ' ', text) # Menggantikan satu atau lebih spasi berturut-turut dengan satu spasi
-    text = text.strip() # Menghapus spasi di awal dan akhir teks
-    text = text.lower() # Mengubah semua teks menjadi huruf kecil
+# Gabungkan Fungsi Cleansing
+def cleansing(s):
+    s = lowercase(s)
+    s = punctuation(s)
+    s = remove_abusive_words(s)
+    s = alay_to_normal(s)
+    return s
 
-    return text
+
 
 # Membuat Interface gradio
 iface = gr.Interface(
-    fn=text_cleansing, 
+    fn=cleansing, 
     inputs=gr.inputs.Textbox(label="Masukan Teks"), 
     outputs=gr.outputs.Textbox(label="Hasil Teks Yang sudah di proses"),
-    title="API Pemrosesan Teks Kasar dan ALay",
+    title="API Documentation for Text Processing and Cleansing",
     description="Dapat digunakan untuk memproses teks yang terdapat kata Kasar dan Alay -dibuat oleh Awan",
     theme="vertical",
     layout="responsive",
